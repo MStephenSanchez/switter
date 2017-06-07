e13638ecadd3038809a67469228ef7ab6ccd203c
MStephenSanchez <mpsanchez3@up.edu.ph>
2017-06-07 16:40:28 +0800

    added create and view swits functionality

--- app/controllers/swits_controller.rb
+++ app/controllers/swits_controller.rb
@@ -1,4 +1,18 @@
 class SwitsController < ApplicationController
   def home
+    @swits = Swit.joins(:user).select('swits.*,users.username')
   end
+
+  def create
+    @swit = Swit.new(swit_params.merge(:user_id => current_user.id))
+
+    @swit.save
+    @swits = Swit.joins(:user).select('swits.*,users.username,users.id as user_id')
+    render 'home'
+  end
+
+  private
+    def swit_params
+      params.require(:swit_form).permit(:message)
+    end
 end
